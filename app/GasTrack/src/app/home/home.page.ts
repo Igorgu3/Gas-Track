c38e050fdfa1f9a415f94e8dd5dacb203e8d8b33
Igor Araujo <igor.souza@equipe.voacolibri.com.br>
2026-07-10 19:12:06 -0300

    Mock-up do aplicativo para posterior conexão com o ESP32

--- app/GasTrack/src/app/home/home.page.ts
+++ app/GasTrack/src/app/home/home.page.ts
@@ -8,5 +8,38 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+
+  public porcentagem: number = 0;
+  public minutosUltimaLeitura: number = 0;
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.minutosUltimaLeitura = 18;
+    this.porcentagem = 20;
+    this.encherBotijao();
+  }
+
+  encherBotijao() {
+
+    const liquido = document.getElementById('liquido') as HTMLElement;
+    let cor = '';
+
+    if (this.porcentagem > 70) {
+      cor = '#22c55e'; // verde
+    } else if (this.porcentagem >= 40) {
+      cor = '#eab308'; // amarelo (40 a 70)
+    } else {
+      cor = '#ef4444'; // vermelho (abaixo de 40)
+    }
+
+    // altura do líquido = a própria porcentagem (ex.: 40 -> 40% do botijão)
+    liquido.style.height = this.porcentagem + '%';
+
+    document.documentElement.style.setProperty(
+      '--cor-liquido',
+      cor
+    );
+  }
 }
